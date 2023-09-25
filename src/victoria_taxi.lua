722adb972f534f7f0c997d0fdd1728ec5119a02a
Kaioru <keith@kaioru.co>
2023-09-26 00:59:51 +0800

    Change code to conform with lua standards

--- src/victoria_taxi.lua
+++ src/victoria_taxi.lua
@@ -1,7 +1,7 @@
 Self.Say("Hello, I drive the Regular Cab. If you want to go from town to town safely and fast, then ride our cab. We'll glady take you to your destination with an affordable price.")
 
-dialog = ""
-cost = 1000
+local dialog = ""
+local cost = 1000
 
 if Target.JobType == 0 then
     dialog = "We have a special 90% discount for beginners. " .. dialog
@@ -9,18 +9,19 @@ if Target.JobType == 0 then
 end
 
 dialog = dialog .. "Choose your destination, for fees will change from place to place."
-fields = { 104000000, 102000000, 101000000, 100000000, 103000000, 120000000 }
-options = { }
+
+local fields = { 104000000, 102000000, 101000000, 100000000, 103000000, 120000000 }
+local options = { }
 
 for key, field in pairs(fields) do
-    if Target.Field != field then
+    if Target.Field ~= field then
         options[field] = "#m" .. field .. "# (" .. cost .. " mesos)"
     end
 end
 
-ret = Self.AskMenu(dialog, options)
+local field = Self.AskMenu(dialog, options)
 
-if not Self.AskYesNo("You don't have anything else to do here, huh? Do you really want to go to #b#m" .. ret .. "##k? It'll cost you #b" .. cost .. " mesos#k.") then
+if not Self.AskYesNo("You don't have anything else to do here, huh? Do you really want to go to #b#m" .. field .. "##k? It'll cost you #b" .. cost .. " mesos#k.") then
     Self.Say("There's a lot to see in this town, too. Come back and find us when you need to go to a different town.")
     return
 end
@@ -31,4 +32,4 @@ if Target.Money < cost then
 end
 
 Target.IncMoney(-cost)
-Target.TransferField(ret)
+Target.TransferField(field)
