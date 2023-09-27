047824d6b7769bca952cff3ee000cafd8f7a1d3f
Kaioru <keith@kaioru.co>
2023-09-28 00:41:16 +0800

    Change Resi_tutor10 script to no offer skipping

--- src/resistance/Resi_tutor10.lua
+++ src/resistance/Resi_tutor10.lua
@@ -1,5 +1,2 @@
-if Self.AskYesNo("Would you like to skip the tutorials and head straight to Edelstein?") then
-    Target.TransferField(310000000)
-else
-    Self.Say("Enjoy your trip.")
-end
+Target.SetStandAloneMode(true)
+-- TODO Unsure
