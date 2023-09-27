96bfaa2b1fbf030c48e39164fc95632c200a313d
Kaioru <keith@kaioru.co>
2023-09-28 00:47:32 +0800

    Add portal sound effect to talk2159011

--- src/resistance/talk2159011.lua
+++ src/resistance/talk2159011.lua
@@ -2,6 +2,7 @@ local Hole = Self.Speaker(2159011)
 
 if Hole.AskYesNo("#b(What a suspicious hole. Maybe Von is hiding inside. Peek inside?)#k") then
     Target.IncEXP(35)
+    Target.EffectPlayPortalSE();
     Target.TransferField(931000010)
 else
     Hole.Say("#b(Even Von wouldn't hide here, right?)#k")
